426edcd9bdd4f967456d68ce374794ac10e40cc1
nouh ben kadi <benkadi.nouh@icloud.com>
2021-11-05 16:16:04 +0100

    added avatar.dart

---
+++ lib/widgets/avatar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Avatar extends StatelessWidget {
+  const Avatar({
+    Key? key,
+    required this.photoURL,
+    required this.radius,
+    this.onPress,
+    this.borderColor,
+    this.borderWidth,
+  }) : super(key: key);
+  final String? photoURL;
+  final double radius;
+  final VoidCallback? onPress;
+  final Color? borderColor;
+  final double? borderWidth;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPress,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          border: Border.all(color: borderColor!, width: borderWidth!),
+          shape: BoxShape.circle,
+        ),
+        child: CircleAvatar(
+          radius: radius,
+          backgroundImage: NetworkImage(photoURL!),
+          backgroundColor: Colors.black12,
+          child: photoURL == null ? const Icon(Icons.add) : null,
+        ),
+      ),
+    );
+  }
+}
