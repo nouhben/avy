c6c6b776694ba4cc78bd61eab7db61995a551ebf
nouh ben kadi <benkadi.nouh@icloud.com>
2021-11-12 22:58:07 +0100

    added the abstraction to auth service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,15 @@
+abstract class AuthService<T> {
+  Future<T> currentUser();
+  Future<T> signInAnon();
+  Future<T> signInWithEmailAndPassword(
+      {required String email, required String password});
+  Future<T> createUserWithEmailAndPassword(
+      {required String email, required String password});
+  Future<void> sendRestPasswordEmail({required String email});
+  Future<T> signInWithGoogle();
+  Future<T> signInWithApple();
+  Future<T> signInWithFacebook();
+  Future<void> signOut();
+  void dispose();
+  Stream<T> get onAuthStateChanged;
+}
